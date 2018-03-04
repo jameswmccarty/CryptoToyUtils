3aba734980f73f6b2547324e7e1adbba84a4c649
James McCarty <jameswmccarty@gmail.com>
2018-03-04 20:13:23 +0900

    Generic Python XOR utility script

---
+++ applyXOR.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+"""
+Example utility function to accept a string (or raw bytes)
+and XOR it with a key.  Key can be of any size, and will
+apply as a repeating key if necessary.
+
+Output is a hex encoded string.
+"""
+
+import base64
+
+#Returns: 	HEX encoded string of rawbytes XOR pad (repeating)
+#Inputs: 	rawbytes - ASCII string to encode
+#			pad - One or more ASCII chars
+def applyXORpad(rawbytes, pad):
+	padidx = 0;
+	output = ""
+	if len(pad) == 0:
+		print "Error: pad cannot be zero length"
+		exit()
+	for byte in rawbytes:
+		output += chr(ord(byte) ^ ord(pad[padidx]))
+		padidx += 1
+		padidx %= len(pad)
+	return base64.b16encode(output)
+
+plaintext = "The quick brown fox jumps over the lazy programmer."
+key = "ABC"
+
+print applyXORpad(plaintext, key)
+
+#Verify
+#base64.b16decode(applyXORpad(base64.b16decode(applyXORpad(plaintext, key)), key))
