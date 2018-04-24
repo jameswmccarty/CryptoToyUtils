50a9b158207ef18881d4a2fb7d16b02c50edef61
James McCarty <jameswmccarty@gmail.com>
2018-04-24 21:36:57 +0900

    Demonstrate breaking HMAC-SHA1 via an artifical timing attack.

---
+++ HMAC_Timing_Attack_Client.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import urllib
+import time
+
+"""
+There is a server with a timing leak.
+
+We want to submit a valid input (filename, signature), but we don't know the signature.
+
+i.e. http://localhost:8080/?filename=foo&signature=46b4ec586117154dacd49d664e5d63fdc88efb51
+
+Goal: Determine a valid signature for a given filename by exploiting the timing leak.
+
+Server returns 'HTTP 500' for an invalid signature.
+Server returns 'HTTP 200' for a valid signature.
+
+Keys that are more correct take longer to compare.
+
+Start with a base key.  Try combinations for the first byte.  See which one took the longest.
+Add that value to our key.  Repeat until HTTP 200 is recieved from server.
+"""
+
+servername = 'localhost'
+port = "8080"
+protocol = 'http'
+
+def build_url(filename, usig):
+	out = protocol + '://'
+	out += servername
+	if port != "80":
+		out += ':' + port
+	out += '/'
+	out += '?filename=' + filename
+	out += '&signature=' + usig
+	return out
+
+def pad(string):
+	return string + "0" * (40 - len(string))
+
+def solve_sig(filename):
+	sig = ''
+	valid = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+
+	best_time = 0.00
+
+	while True:
+		best = ''
+		for char in valid:
+			trial = sig + char
+			print "Testing sig: " + pad(trial)
+			start = time.time()
+			status = urllib.urlopen(build_url(filename,pad(trial)))
+			stop = time.time()
+			delta = stop-start
+			#print delta
+			code = status.getcode()
+			if code == None:
+				print "Unknown Server Error."
+				exit()
+			if code == 200:
+				return trial
+			if code == 500:
+				if delta >= best_time:
+					best_time = delta
+					best = char
+		sig += best
+
+if __name__ == "__main__":
+	print solve_sig("foo")
---
+++ HMAC_Timing_Attack_Server.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import web
+import sys
+import HMAC_SHA1 as hmac
+import time
+
+"""
+Demonstrate breaking HMAC-SHA1 on a webserver
+using an (artificial) timing attack
+"""
+
+""" This module serves as an insecure web server """
+
+
+urls = ('/', 'index',
+		'/debug/', 'debug')
+
+server_key = 'foobarfizzbuzz' # unknown to the outside
+
+class FileValidator:
+	# timing leak introduced
+	# compare char-by-char our given sig and server sig
+	def insecure_compare(self, fname_, usig_):
+		ssig_ = hmac.HMAC_SHA1(server_key, fname_)		
+		for i in range(0, len(usig_)):
+			if ssig_[i] != usig_[i]:
+				return False
+			time.sleep(0.05) # whoops!
+		return True # sig is valid
+
+	# for debugging, generate valid server keys
+	def get_sig(self, fname_):
+		return hmac.HMAC_SHA1(server_key, fname_)
+
+# Takes a "file" argument and a "signature" argument, i.e.:
+# http://localhost:8080/?filename=foo&signature=46b4ec586117154dacd49d664e5d63fdc88efb51
+# Compares signature to file HMAC
+# Returns a 500 if the MAC is invalid, and a 200 if it's OK.
+class index:
+	def GET(self):
+		i = web.input(filename="", signature="")
+		if i.filename == "" or i.signature == "":
+			return "Example - http://localhost:8080/?filename=foo&signature=46b4ec586117154dacd49d664e5d63fdc88efb51"
+		if FileValidator().insecure_compare(i.filename, i.signature):
+			return web.ok() # 200 error
+		else:
+			return web.InternalError(message=None) # 500 error
+
+# Takes a "file" argument and return a valid Server Signature:
+# http://localhost:8080/debug/?filename=foo
+# only here for testing
+# http://localhost:8080/?filename=foo&signature=9e1d9f3ef7abc6d7e51366e4c7834e92b5bd1c71 #valid
+class debug:
+	def GET(self):
+		i = web.input(filename="")
+		return FileValidator().get_sig(i.filename)		
+        
+
+if __name__ == "__main__":
+	#reload(sys)
+	#sys.setdefaultencoding('utf-8')
+	web.config.debug = False
+	app = web.application(urls, globals())
+	app.run()
