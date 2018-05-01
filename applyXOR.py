4d7a041abef65b93da7a317368791cb57a30346e
James McCarty <jameswmccarty@gmail.com>
2018-05-01 20:02:35 +0900

    Cleanup functions using enumerate().

--- applyXOR.py
+++ applyXOR.py
@@ -4,8 +4,6 @@
 Example utility function to accept a string (or raw bytes)
 and XOR it with a key.  Key can be of any size, and will
 apply as a repeating key if necessary.
-
-Output is a hex encoded string.
 """
 
 import base64
@@ -14,31 +12,25 @@ import base64
 #Inputs: 	rawbytes - ASCII string to encode
 #			pad - One or more ASCII chars
 def rawXORpad(rawbytes, pad):
-	padidx = 0;
-	output = ""
+	output = b''	
 	if len(str(pad)) == 0:
 		print "Error: pad cannot be zero length"
 		exit()
-	for byte in rawbytes:
-		output += chr(ord(byte) ^ ord(pad[padidx]))
-		padidx += 1
-		padidx %= len(pad)
-	return output
+	for i,byte in enumerate(rawbytes):
+		output += chr(ord(byte) ^ ord(pad[i%len(pad)]))
+	return output		
 
 #Returns: 	HEX encoded string of rawbytes XOR pad (repeating)
 #Inputs: 	rawbytes - ASCII string to encode
 #			pad - One or more ASCII chars
 def applyXORpad(rawbytes, pad):
-	padidx = 0;
-	output = ""
+	output = b''	
 	if len(str(pad)) == 0:
 		print "Error: pad cannot be zero length"
 		exit()
-	for byte in rawbytes:
-		output += chr(ord(byte) ^ ord(pad[padidx]))
-		padidx += 1
-		padidx %= len(pad)
-	return base64.b16encode(output)
+	for i,byte in enumerate(rawbytes):
+		output += chr(ord(byte) ^ ord(pad[i%len(pad)]))
+	return base64.b16encode(output)	
 
 #Testing below
 if __name__ == "__main__":
